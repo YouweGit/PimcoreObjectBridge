f502a69ec1578886c6d9ecc5a31485e877f7cbe3
Joris Ros <joris.ros@gmail.com>
2018-02-09 16:48:44 +0100

    Add composer package name

--- ObjectBridgeBundle.php
+++ ObjectBridgeBundle.php
@@ -3,9 +3,19 @@
 namespace ObjectBridgeBundle;
 
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
 
 class ObjectBridgeBundle extends AbstractPimcoreBundle
 {
+    use PackageVersionTrait;
+    
+    protected function getComposerPackageName(): string
+    {
+        // getVersion() will use this name to read the version from
+        // PackageVersions and return a normalized value
+        return 'youwe/pimcore-object-bridge';
+    }
+    
     public function getJsPaths() {
         return [
             '/bundles/objectbridge/js/pimcore/objects/tags/objectBridge.js',
